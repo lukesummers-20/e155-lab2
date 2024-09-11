6108654144722d5a85945190db7aa000ba494998
Luke Summers <lsummers@g.hmc.edu>
2024-09-10 22:27:29 -0700

    testbench and wave pics

--- fpga/sim/lab2_inputMultiplexer_tb.sv
+++ fpga/sim/lab2_inputMultiplexer_tb.sv
@@ -5,45 +5,52 @@ module lab2_inputMultiplexer_tb();
     logic en2, en2Expected;
     logic clk, reset;
 
-    inputMultiplexer dut(s1, s2, clk, reset, en1, en2, sevSegInput);
+    inputMultiplexer dut(reset, clk, s1, s2, en1, en2, sevSegInput);
     initial begin
-        s1 = 4'b0010;
-        s2 = 4'b1000;
-        sevSegInputExpected = 4'b0010;
-        en1Expected = 0;
-        en2Expected = 1;
-        reset = 0;
-        #20;
-        reset = 1;
-        if (sevSegInput != sevSegInputExpected) begin
+        clk = 0;
+	reset = 0;
+	s1 = 4'b1111;
+	s2 = 4'b1010;
+	sevSegInputExpected = 4'b1111;
+	en1Expected = 1;
+	en2Expected = 0;
+	#10;
+	clk  = 1;
+	#10;
+	if (sevSegInput != sevSegInputExpected) begin
             $display(" sevSegInput = %b sevSegInputExpected = %b with s1 = %b and s2 = %b", sevSegInput, sevSegInputExpected, s1, s2);
         end if (en1 != en1Expected) begin
             $display(" en1 = %b en1Expected = %b with s1 = %b and s2 = %b", en1, en1Expected, s1, s2);
         end if (en2 != en2Expected) begin
             $display(" en2 = %b en2Expected = %b with s1 = %b and s2 = %b", en2, en2Expected, s1, s2);
         end
-        clk = 0;#5;
-        s1 = 4'b0000;
-        s2 = 4'b1111;
-        en1Expected = 1;
-        en2Expected = 0;
-        sevSegInputExpected = 4'b1111;
-        clk = 1;#10;
-        if (sevSegInput != sevSegInputExpected) begin
+	clk = 0;
+	reset = 1; 
+	s1 = 4'b0110;
+	s2 = 4'b1001;
+	en1Expected = 0;
+	en2Expected = 1;
+	sevSegInputExpected = 4'b1001;
+	#10;
+	clk = 1;
+	#10;
+	if (sevSegInput != sevSegInputExpected) begin
             $display(" sevSegInput = %b sevSegInputExpected = %b with s1 = %b and s2 = %b", sevSegInput, sevSegInputExpected, s1, s2);
         end if (en1 != en1Expected) begin
             $display(" en1 = %b en1Expected = %b with s1 = %b and s2 = %b", en1, en1Expected, s1, s2);
         end if (en2 != en2Expected) begin
             $display(" en2 = %b en2Expected = %b with s1 = %b and s2 = %b", en2, en2Expected, s1, s2);
         end
-        clk = 0;#5;
-        s1 = 4'b0110;
-        s2 = 4'b1001;
-        en1Expected = 0;
-        en2Expected = 1;
-        sevSegInputExpected = 4'b0110;
-        clk = 1;#10;
-        if (sevSegInput != sevSegInputExpected) begin
+	clk = 0; 
+	s1 = 4'b1100;
+	s2 = 4'b0011;
+	en1Expected = 1;
+	en2Expected = 0;
+	sevSegInputExpected = 4'b1100;
+	#10;
+	clk = 1;
+	#10;
+	if (sevSegInput != sevSegInputExpected) begin
             $display(" sevSegInput = %b sevSegInputExpected = %b with s1 = %b and s2 = %b", sevSegInput, sevSegInputExpected, s1, s2);
         end if (en1 != en1Expected) begin
             $display(" en1 = %b en1Expected = %b with s1 = %b and s2 = %b", en1, en1Expected, s1, s2);
--- fpga/src/inputMultiplexer.sv
+++ fpga/src/inputMultiplexer.sv
@@ -1,17 +1,17 @@
 module inputMultiplexer(
     input reset, clk,
-    input [3:0] s1, s2,
+        input [3:0] s1, s2,
     output logic en1, en2,
     output logic [3:0] sevSegInput
 );
     assign en2 = ~en1;
     always_ff @(posedge clk) begin
         if (reset == 0) begin
-            en1 <= 0;
-            sevSegInput = s1;
+            en1 <= 1;
+            sevSegInput <= s1;
         end else begin
-            if (en1) sevSegInput = s2;
-            else sevSegInput = s1;
+            if (en1) sevSegInput <= s2;
+            else sevSegInput <= s1;
             en1 <= ~en1;
         end
     end
